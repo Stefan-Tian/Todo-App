7ef757c42c31bca2fe4ba4f82144c27f8162f3d2
Sheng Hong Tian <stefan55831@gmail.com>
2017-12-19 09:44:14 +0800

    Add get/delete single todo route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -12,24 +12,57 @@ module.exports = app => {
       _user: req.user.id
     });
 
-    todo.save().then(
-      doc => {
-        res.send(doc);
-      },
-      e => {
-        res.status(400).send(e);
-      }
-    );
+    todo.save().then(doc => res.send(doc), e => res.status(400).send(e));
   });
 
   app.get("/api/todos", requireLogin, (req, res) => {
     Todo.find({ _user: req.user.id }).then(
-      todos => {
-        res.send({ todos });
+      todos => res.send({ todos }),
+      e => res.status(400).send(e)
+    );
+  });
+
+  app.get("/api/todos/:id", requireLogin, (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+
+    Todo.findOne({
+      _id: id,
+      _user: req.user.id
+    }).then(
+      todo => {
+        if (todo) {
+          return res.send({ todo });
+        }
+
+        res.status(404).send();
       },
-      e => {
-        res.status(400).send(e);
-      }
+      e => res.status(400).send(e)
+    );
+  });
+
+  app.delete("/api/todos/:id", requireLogin, (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+
+    Todo.findOneAndRemove({
+      _id: id,
+      _user: req.user.id
+    }).then(
+      todo => {
+        if (todo) {
+          return res.send({ todo });
+        }
+
+        res.status(404).send();
+      },
+      e => res.status(400).send(e)
     );
   });
 };
