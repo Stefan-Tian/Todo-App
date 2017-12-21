29af54232e2632a1a362bc3f55c0b089554a0eb7
Sheng Hong Tian <stefan55831@gmail.com>
2017-12-21 21:53:37 +0800

    Add patch route for single todo

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const requireLogin = require("../middlewares/requireLogin");
 
 const Todo = mongoose.model("Todo");
@@ -48,7 +49,7 @@ module.exports = app => {
     const id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
+      return res.status(404).send("ID not found");
     }
 
     Todo.findOneAndRemove({
@@ -64,5 +65,42 @@ module.exports = app => {
       },
       e => res.status(400).send(e)
     );
+  });
+
+  app.patch("/api/todos/:id", requireLogin, (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ["title", "text", "completed"]); // limited items that can be updated
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send("Invalid ID");
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    Todo.findOneAndUpdate(
+      {
+        _id: id,
+        _user: req.user.id
+      },
+      {
+        $set: body
+      },
+      {
+        new: true
+      }
+    )
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send();
+        }
+
+        res.send({ todo });
+      })
+      .catch(e => res.status(404).send(e));
   });
 };
